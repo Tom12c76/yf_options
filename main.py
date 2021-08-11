650270dc2b33dd0702dee403c9d642d6aae1974e
Tom12c76 <thomascastri@gmail.com>
2021-08-11 12:12:47 +0200

    various chgs formatting mainly

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ from plotly.subplots import make_subplots
 ### FORMATTING
 st.set_page_config(page_title="TC's Opt Screen", layout="wide")
 st.sidebar.header("TC's Option Screener :sunglasses:")
-# st.write('Hello, *World!* :sunglasses:')
 
-# askdfjhka
-
-
+### CONSTANTS
 snsgreen, snsorange, snsred, snsblue, snsgrey = ['#55a868','#ff7f0e','#c44e52','#4c72b0','#8c8c8c']
 irange  = range(5)
 width   = [1.25,1.25,1.25,1.25,1.25]
 opacity = [1,1,0.5,0.75,1]
-ticker_list = ['SB', 'DSX', 'AMC', 'AAPL', 'MSFT', 'AMZN', 'FB', 'GOOGL', 'GOOG', 'BRK-B', 'TSLA', 'NVDA', 'JPM', 'JNJ', 'V', 'UNH', 'HD', 'PG', 'PYPL', 'MA', 'DIS', 'BAC', 'ADBE', 'CMCSA', 'PFE', 'XOM', 'CRM', 'CSCO', 'NFLX', 'VZ', 'NKE', 'KO', 'INTC', 'ABT', 'PEP', 'TMO', 'LLY', 'ACN', 'ABBV', 'WFC', 'T', 'WMT', 'AVGO', 'CVX', 'DHR', 'COST', 'MRK', 'TXN', 'MCD', 'MDT', 'QCOM', 'ORCL', 'HON', 'LIN', 'NEE', 'PM', 'BMY', 'MS', 'C', 'UNP', 'INTU', 'SBUX', 'UPS', 'GS', 'LOW', 'AMD', 'RTX', 'AMGN', 'AMAT', 'IBM', 'TGT', 'AMT', 'BA', 'BLK', 'MRNA', 'ISRG', 'NOW', 'GE', 'MMM', 'DE', 'CAT', 'AXP', 'SCHW', 'CVS', 'SPGI', 'CHTR', 'PLD', 'ZTS', 'ANTM', 'LRCX', 'MU', 'ADP']
 
 ### FUNCTIONS
 @st.cache
@@ -75,10 +71,11 @@ def plot_payoff():
     fig = go.Figure()
 
     fig = make_subplots(rows=2, cols=2, shared_xaxes=True, shared_yaxes=True,
-                        vertical_spacing=0.015, horizontal_spacing=0.01,
+                        vertical_spacing=0.05, horizontal_spacing=0.01,
                         row_heights=[2, 1], column_widths=[8, 2],
-                        column_titles=[f'<b>{long_short} {lots} lots of <br>{exp_date}  {ticker}  {strike}  strike  {strategy}s  @  {str(lastPrice)}', f'Premium Paid = {lots * mult * lastPrice:,.0f}'],
-                        row_titles=['Profit & Loss', 'Rolling Ret & Backtest'])
+                        column_titles=[f'<b>{long_short} {lots} lots of <br>{exp_date}  {ticker}  {strike}  strike  {strategy}s  @  {str(lastPrice)}', f'<b>Premium Paid = {lots * mult * lastPrice:,.0f}'],
+                        # row_titles=['<b>Profit & Loss', '<b>Rolling Ret & Backtest'],
+                        subplot_titles = ('', '', '<b>Rolling Return & Backtest'))
 
     fig.add_trace(go.Scatter(x=df_hist.index, y=df_hist[ticker],
                              name=ticker, connectgaps=True, line={'color': snsblue, 'width': 2.5}, opacity=0.8), row=1, col=1)
@@ -112,8 +109,8 @@ def plot_payoff():
 
         fig.add_trace(go.Scatter(
             x=[df_hist.index.min(), exp_date],
-            y=[(strike + i * pcf * lastPrice) / df_hist[ticker].iloc[-1] - 1,
-               (strike + i * pcf * lastPrice) / df_hist[ticker].iloc[-1] - 1],
+            y=[(strike + i * pcf * lastPrice) / df_hist[ticker].iloc[0] - 1,
+               (strike + i * pcf * lastPrice) / df_hist[ticker].iloc[0] - 1],
             showlegend=False, mode='lines', opacity=o, line={'color': color, 'width': w, 'dash': 'dashdot'}), row=2, col=1)
 
     fig.add_trace(go.Scatter(x=np.multiply([-1, -1, 0, 1, 2, 3], lastPrice) * lots * mult,
@@ -129,7 +126,8 @@ def plot_payoff():
                              name=str(td2e) + ' td Rol Ret', connectgaps=True, line={'color': snsgrey, 'width': 2}), row=2, col=1)
 
     fig.update_xaxes(zerolinecolor='grey', zerolinewidth=1.25, col=2, row=1)
-    fig.update_yaxes(zerolinecolor='grey', zerolinewidth=1.25, col=1, row=2)
+    fig.update_yaxes(zerolinecolor='grey', zerolinewidth=1.25, tickformat='%', col=1, row=2)
+    #fig.update_layout(yaxis_tickformat='%')
     fig.update_layout(margin=dict(l=0, r=0, t=60, b=0), template='seaborn', plot_bgcolor='#F0F2F6')
     fig.update_layout(height=700, width=1400)
     return fig
@@ -139,18 +137,15 @@ def plot_payoff():
 
 ticker = st.sidebar.text_input('Enter US stock ticker','AAPL')
 
-#     if (yf.Ticker(ticker).info['regularMarketPrice'] == None):
-#         raise NameError("You did not input a correct stock ticker! Try again.")
+st.sidebar.write(f'Stock summary on [yahoo!] (https://finance.yahoo.com/quote/{ticker})')
 
 df_hist = get_hist(ticker)
-
 exp_dates = get_exp_dates(ticker)
 exp_date = st.sidebar.selectbox('Pick exp date', exp_dates)
 exp_date = datetime.datetime.strptime(exp_date, '%Y-%m-%d').date()
 today_date = df_hist.index.max().date()
 cd2e = (exp_date - today_date).days
 td2e = cd2e//7*5 + cd2e%7
-#st.write(today_date, exp_date, cd2e, td2e)
 
 call_chain, put_chain = get_chains(ticker)
 strategy = st.sidebar.radio('Strategy', ('Call', 'Put'))
